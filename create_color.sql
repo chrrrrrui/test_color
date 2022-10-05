34032ab0deea87b800da6b18ef7f2c4183286050
詹惠茹 <58292178+chrrrrrui@users.noreply.github.com>
2022-10-05 22:43:35 +0800

    Add files via upload

---
+++ create_color.sql
@@ -0,0 +1,50 @@
+drop table color
+
+create table color
+(
+
+    Id bigint PRIMARY KEY NOT NULL IDENTITY,-- 編號 ( 1, 2, 3 ... )
+    ColorEngName varchar(30) NOT NULL,-- 顏色名稱(英文)
+    ColorName nvarchar(30)-- 顏色名稱(中文)
+
+);
+
+select *
+from color
+
+insert color
+values
+    ('Undefined', NULL)--1
+insert color
+values
+    ('BLUE', '藍色')--2
+insert color
+values
+    ('BLACK', '黑色')--3
+insert color
+values
+    ('WHITE', '白色')--4
+insert color
+values
+    ('RED', '紅色')--5
+insert color
+values
+    ('ORANGE', '橘色')--6
+insert color
+values
+    ('YELLOW', '黃色')--7
+insert color
+values
+    ('GREEN', '綠色')--8
+insert color
+values
+    ('PURPLE', '紫色')--9
+insert color
+values
+    ('GRAY', '灰色')--10
+insert color
+values
+    ('PINK', '粉紅色')--11
+insert color
+values
+    ('CYAN', '青色') --12
